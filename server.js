903a193813db98a34c719e5cff06371d8db16d13
Elam W <tedmanz2003@gmail.com>
2024-04-27 00:55:19 -0500

    fixed typo in server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api', authRoutes)
 app.use('/api', postRoutes)
 
 
-const PORT = proces.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
