abc1beca5efd22f12f05ec16e8b3e97ef8b8c419
Elam W <tedmanz2003@gmail.com>
2024-04-27 12:26:37 -0500

    removed local host from cors

--- server/app.js
+++ server/app.js
@@ -12,9 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+app.use(cors());
 
 app.use(express.json());
 
