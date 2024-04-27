085f578f79c39a4b826054ee0fcb07efd46a6cda
Elam W <tedmanz2003@gmail.com>
2024-04-27 01:55:38 -0500

    fixed another routing issue

--- server.js
+++ server.js
@@ -7,25 +7,25 @@ const postRoutesPromise = import ('./server/routes/posts.js');
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/favicon.ico', (req, res) => {
+app.get('/tendytalk/favicon.ico', (req, res) => {
     res.status(204).end()
 })
 
-app.get('/', (req, res) => {
+app.get('/tendytalk/', (req, res) => {
     res.status(204).end()
 })
 
 authRoutesPromise.then(authRoutesModule => {
     const authRoutes = authRoutesModule.default;
-    app.use('/api', authRoutes)
+    app.use('/tendytalk/api', authRoutes)
 })
 postRoutesPromise.then(postRoutesModule => {
     const postRoutes = postRoutesModule.default;
-    app.use('/api', postRoutes)
+    app.use('/tendytalk/api', postRoutes)
 })
 
 
-app.get('*', (req, res) => {
+app.get('/tendytalk/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
